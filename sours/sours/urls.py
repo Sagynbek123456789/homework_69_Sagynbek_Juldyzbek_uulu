5e5d1b59ad974102e488a56433856c47aad91d8c
Sagynbek <jyldyzbekov-s@mail.ru>
2024-02-02 18:33:39 +0600

    Добавил два приложения в URLS проекта

--- sours/sours/urls.py
+++ sours/sours/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+urls_app = [
+    path('v1/', include('calculator.urls'))
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(urls_app)),
+    path('', include('webapp.urls')),
 ]
+
+
