c25bfc6f741d6761fcacbe45e21e9a0ab73ac48d
Sagynbek <jyldyzbekov-s@mail.ru>
2024-02-05 21:21:36 +0600

    перименовал приложение

--- sours/calculator/urls.py
+++ sours/calculator/urls.py
@@ -1,13 +1,30 @@
-from django.urls import path
-from calculator.views import add_view, get_token_view, subtract_view, multiply_view, divide_view
+"""
+URL configuration for sours project.
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
-app_name = 'calculator'
+urls_app = [
+    path('v1/', include('calculator.urls'))
+]
 
 urlpatterns = [
-    path('add/', add_view, name='add_view'),
-    path('subtract/', subtract_view, name='subtract_view'),
-    path('multiply/', multiply_view, name='multiply_view'),
-    path('divide/', divide_view, name='divide_view'),
-    path('token/', get_token_view, name='get_token'),
+    path('admin/', admin.site.urls),
+    path('api/', include(urls_app)),
+    path('', include('webapp.urls')),
 ]
+
+
